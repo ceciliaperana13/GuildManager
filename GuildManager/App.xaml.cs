d2a3824fa250ed7c215c367e8a353bcecf65f35f
ceciliaperana13 <cperana13@gmail.com>
2026-09-09 12:09:59 +0200

    app.xaml changer pour création de l'api et la bdd

--- GuildManager/App.xaml.cs
+++ GuildManager/App.xaml.cs
@@ -1,13 +1,41 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using GuildManager.Infrastructure.Configurations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MonProjet;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
+
 public partial class App : Application
 {
-}
+    private WebApplication? _apiApp;
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+        {
+            ContentRootPath = AppContext.BaseDirectory
+        });
+
+        builder.Services.AddControllers();
+        builder.Services.AddInfrastructure(builder.Configuration);
+
+        _apiApp = builder.Build();
+        _apiApp.MapControllers();
+
+        _ = _apiApp.RunAsync("http://0.0.0.0:5080");
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        if (_apiApp is not null)
+            await _apiApp.StopAsync();
+
+        base.OnExit(e);
+    }
+}
