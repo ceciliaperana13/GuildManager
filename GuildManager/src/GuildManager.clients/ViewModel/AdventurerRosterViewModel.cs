848304105f24b231d8398b86ecce47359f0d5308
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-09-21 08:59:53 +0200

    debut affichage aventurier

---
+++ GuildManager/src/GuildManager.clients/ViewModel/AdventurerRosterViewModel.cs
@@ -0,0 +1,20 @@
+using System.Collections.ObjectModel;
+
+namespace GuildManager.Client.ViewModel;
+
+public class AdventurerRosterViewModel : IScreenViewModel
+{
+    public string BackgroundPath => "/Assets/Backgrounds/table_avanturier.png";
+
+    public ObservableCollection<Adventurer> Adventurers { get; } = new();
+
+   public AdventurerRosterViewModel()
+{
+    // TODO: remplacer par une vraie requête EF Core sur GuildManagerDbContext
+    // (ClassName sera fourni par la logique de classes d'un autre membre de l'équipe)
+
+    Adventurers.Add(new Adventurer { Name = "???", Level = 1, Status = AdventurerStatus.Disponible, PortraitPath = "/Assets/UI/placeholder_portrait.png" });
+    Adventurers.Add(new Adventurer { Name = "???", Level = 2, Status = AdventurerStatus.EnQuete, PortraitPath = "/Assets/UI/placeholder_portrait.png" });
+    Adventurers.Add(new Adventurer { Name = "???", Level = 1, Status = AdventurerStatus.Blesse, PortraitPath = "/Assets/UI/placeholder_portrait.png" });
+}
+}
