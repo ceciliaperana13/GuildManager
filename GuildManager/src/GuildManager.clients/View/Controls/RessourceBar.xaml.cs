559a0d680a5edb9db2047aac350080b60d123e75
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-09-21 14:17:52 +0200

    rajout de la barre hud

---
+++ GuildManager/src/GuildManager.clients/View/Controls/RessourceBar.xaml.cs
@@ -0,0 +1,30 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GuildManager.Client.View.Controls;
+
+public partial class ResourceBar : UserControl
+{
+    public static readonly DependencyProperty GoldProperty =
+        DependencyProperty.Register(nameof(Gold), typeof(int), typeof(ResourceBar));
+
+    public static readonly DependencyProperty FoodProperty =
+        DependencyProperty.Register(nameof(Food), typeof(int), typeof(ResourceBar));
+
+    public int Gold
+    {
+        get => (int)GetValue(GoldProperty);
+        set => SetValue(GoldProperty, value);
+    }
+
+    public int Food
+    {
+        get => (int)GetValue(FoodProperty);
+        set => SetValue(FoodProperty, value);
+    }
+
+    public ResourceBar()
+    {
+        InitializeComponent();
+    }
+}
