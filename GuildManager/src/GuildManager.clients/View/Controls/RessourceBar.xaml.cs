6d41432653da529f67e3c89a069203a51a626df2
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-09-22 14:21:07 +0200

    changement de la barre UID besoin de régler l'affichage

--- GuildManager/src/GuildManager.clients/View/Controls/RessourceBar.xaml.cs
+++ GuildManager/src/GuildManager.clients/View/Controls/RessourceBar.xaml.cs
@@ -7,21 +7,14 @@ public partial class ResourceBar : UserControl
 {
     public static readonly DependencyProperty GoldProperty =
         DependencyProperty.Register(nameof(Gold), typeof(int), typeof(ResourceBar));
-
     public static readonly DependencyProperty FoodProperty =
         DependencyProperty.Register(nameof(Food), typeof(int), typeof(ResourceBar));
+    public static readonly DependencyProperty PrestigeProperty =
+        DependencyProperty.Register(nameof(Prestige), typeof(int), typeof(ResourceBar));
 
-    public int Gold
-    {
-        get => (int)GetValue(GoldProperty);
-        set => SetValue(GoldProperty, value);
-    }
-
-    public int Food
-    {
-        get => (int)GetValue(FoodProperty);
-        set => SetValue(FoodProperty, value);
-    }
+    public int Gold { get => (int)GetValue(GoldProperty); set => SetValue(GoldProperty, value); }
+    public int Food { get => (int)GetValue(FoodProperty); set => SetValue(FoodProperty, value); }
+    public int Prestige { get => (int)GetValue(PrestigeProperty); set => SetValue(PrestigeProperty, value); }
 
     public ResourceBar()
     {
